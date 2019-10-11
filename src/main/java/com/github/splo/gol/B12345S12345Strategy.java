64fb66f836e9a2ea07190457195b5920a0a15783
Sébastien Job <sebastien.job@gmail.com>
2019-10-11 16:06:41 +0200

    Add a B12345/S12345 strategy
    A strategy where a cell is born or survives if there are between 1 and 5
    living cells around.

---
+++ src/main/java/com/github/splo/gol/B12345S12345Strategy.java
@@ -0,0 +1,15 @@
+package com.github.splo.gol;
+
+/**
+ * A strategy where a cell is born or survives if there are between 1 and 5 living cells around.
+ */
+public class B12345S12345Strategy implements Strategy {
+    @Override
+    public CellState getNextCellState(final CellState previousState, final long livingCells) {
+        if (livingCells >= 1 && livingCells <= 5) {
+            return CellState.ALIVE;
+        } else {
+            return CellState.DEAD;
+        }
+    }
+}
---
+++ src/test/java/com/github/splo/gol/B12345S12345StrategyTest.java
@@ -0,0 +1,50 @@
+package com.github.splo.gol;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class B12345S12345StrategyTest {
+
+    private B12345S12345Strategy strategy;
+
+    @BeforeEach
+    void setUp() {
+        strategy = new B12345S12345Strategy();
+    }
+
+    @Test
+    void bornConditions() {
+        assertThat(strategy.getNextCellState(CellState.DEAD, 1)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.DEAD, 2)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.DEAD, 3)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.DEAD, 4)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.DEAD, 5)).isEqualTo(CellState.ALIVE);
+    }
+
+    @Test
+    void stayDeadConditions() {
+        assertThat(strategy.getNextCellState(CellState.DEAD, 0)).isEqualTo(CellState.DEAD);
+        assertThat(strategy.getNextCellState(CellState.DEAD, 6)).isEqualTo(CellState.DEAD);
+        assertThat(strategy.getNextCellState(CellState.DEAD, 7)).isEqualTo(CellState.DEAD);
+        assertThat(strategy.getNextCellState(CellState.DEAD, 8)).isEqualTo(CellState.DEAD);
+    }
+
+    @Test
+    void stayAliveConditions() {
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 1)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 2)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 3)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 4)).isEqualTo(CellState.ALIVE);
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 5)).isEqualTo(CellState.ALIVE);
+    }
+
+    @Test
+    void deathConditions() {
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 0)).isEqualTo(CellState.DEAD);
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 6)).isEqualTo(CellState.DEAD);
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 7)).isEqualTo(CellState.DEAD);
+        assertThat(strategy.getNextCellState(CellState.ALIVE, 8)).isEqualTo(CellState.DEAD);
+    }
+}
