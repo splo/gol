b140062f4c8cc9f8a02d7dae0a2c96b1e3775bb9
Sébastien Job <sebastien.job@gmail.com>
2019-10-11 16:50:48 +0200

    Add a simple socket client and server that accepts multiple connections

---
+++ src/main/java/com/github/splo/gol/SocketClient.java
@@ -0,0 +1,33 @@
+package com.github.splo.gol;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SocketClient {
+
+    private static final long TIMEOUT_MS = 5000;
+
+    public void run(final String host, final int port) {
+        try (final Socket socket = new Socket(host, port);
+                final BufferedInputStream input = new BufferedInputStream(socket.getInputStream())) {
+            System.out.println("Connected to " + socket.getInetAddress());
+            long lastReadTimeMs = System.currentTimeMillis();
+            while (System.currentTimeMillis() - lastReadTimeMs < TIMEOUT_MS) {
+                if (input.available() > 0) {
+                    // First read the data size in bytes.
+                    final int dataLength = input.read();
+                    final byte[] buffer = new byte[dataLength];
+                    // Then read the whole grid.
+                    input.read(buffer);
+                    lastReadTimeMs = System.currentTimeMillis();
+                    final Grid grid = GridCodec.decodeGrid(buffer);
+                    System.out.println(grid);
+                }
+            }
+            System.out.println("Server connection timed out");
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/github/splo/gol/SocketServer.java
@@ -0,0 +1,52 @@
+package com.github.splo.gol;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SocketServer {
+
+    private final List<Socket> clientSockets = new ArrayList<>();
+
+    public void run(final int port) {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final Socket clientSocket = serverSocket.accept();
+                System.out.println("New connection from " + clientSocket.getRemoteSocketAddress());
+                synchronized (clientSockets) {
+                    clientSockets.add(clientSocket);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendGrid(final Grid grid) {
+        final byte[] encodedGrid = GridCodec.encodeGrid(grid);
+        synchronized (clientSockets) {
+            final List<Socket> closedSockets = new ArrayList<>();
+            clientSockets.forEach(socket -> {
+                try {
+                    final OutputStream outputStream = socket.getOutputStream();
+                    // First send the data size in bytes.
+                    outputStream.write(encodedGrid.length);
+                    // TODO: Improve the protocol to allow grids larger than 255 bytes.
+                    // Then send the whole grid.
+                    outputStream.write(encodedGrid);
+                } catch (IOException e) {
+                    System.out.println("Client disconnected: " + socket.getRemoteSocketAddress());
+                    try {
+                        socket.close();
+                    } catch (final IOException ignored) {
+                    }
+                    closedSockets.add(socket);
+                }
+            });
+            clientSockets.removeAll(closedSockets);
+        }
+    }
+}
